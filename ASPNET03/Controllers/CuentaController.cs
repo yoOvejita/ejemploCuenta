959c6f6dbbfac4ff1c39cac39a4cee790fb5d04f
Ruben Gonzalo Soria Soria <rusovsky_c@hotmail.com>
2020-05-25 13:51:03 -0400

    Segundo commit, se agregan todos los cambios para que funcione el proyecto.

---
+++ ASPNET03/Controllers/CuentaController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ASPNET03.Controllers
+{
+    public class CuentaController : Controller
+    {
+        // GET: Cuenta
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(string name, string password)
+        {
+            if (name.Equals("admin") && password.Equals("admin"))
+            {
+                Session["username"] = name;
+                return View("Success");
+            }
+            else {
+                ViewBag.error = "Usuario y/o password inválido";
+                return View("Index");
+            }
+            
+        }
+        [HttpGet]
+        public ActionResult Logout()
+        {
+            Session.Remove("username");
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ ASPNET03/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ASPNET03.MvcApplication" Language="C#" %>
